a0ccf8fa24ff52c715d442aeff530facfc8f58e7
Alysty <pppjw@protonmail.ch>
2021-07-04 17:45:59 -0300

    teste passando 1 test

---
+++ Calculadora.js
@@ -0,0 +1,7 @@
+export function calculadora(emp){
+    if(emp.role === "DESENVOLVEDOR"){
+        if(emp.salary > 3000 ){
+            return emp.salary * 0.80
+        }
+    }
+}
---
+++ Funcionarios.js
@@ -0,0 +1,8 @@
+export default class Funcionarios{
+    constructor(name, email, salary, role){
+        this.name = name
+        this.email = email
+        this.salary = salary
+        this.role = role
+    }
+}
