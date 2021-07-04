05da41b0578d5ee270758251b882a4459e5f6560
Alysty <pppjw@protonmail.ch>
2021-07-04 18:29:00 -0300

    teste falhando 5 6 and 7 tests

--- __tests__/Tests.spec.js
+++ __tests__/Tests.spec.js
@@ -16,7 +16,24 @@ test('Deve retornar o salario liquido de um DESENVOLVERDOR com um é salario igu
     const emp = new Funcionarios("name", "email", 3000, "DESENVOLVEDOR")
     expect(calculadora(emp)).toBe(2400)
 })
+
 test('Deve retornar o salario liquido de um DESENVOLVERDOR com um é salario menor que 3000',()=>{
     const emp = new Funcionarios("name", "email", 2000, "DESENVOLVEDOR")
     expect(calculadora(emp)).toBe(1800)
 })
+
+test('Deve retornar o salario liquido de um DBA com um é salario maior que 2000',()=>{
+    const emp = new Funcionarios("name", "email", 4000, "DBA")
+    expect(calculadora(emp)).toBe(3000)
+})
+
+test('Deve retornar o salario liquido de um DBA com um é salario igual a 2000',()=>{
+    const emp = new Funcionarios("name", "email", 2000, "DBA")
+    expect(calculadora(emp)).toBe(1500)
+})
+
+test('Deve retornar o salario liquido de um DBA com um é salario menor que 2000',()=>{
+    const emp = new Funcionarios("name", "email", 1000, "DBA")
+    expect(calculadora(emp)).toBe(850)
+})
+
