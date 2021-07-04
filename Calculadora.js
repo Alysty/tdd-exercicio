2ee5eb669f846459860f47e832c4724e1d970459
Alysty <pppjw@protonmail.ch>
2021-07-04 17:47:38 -0300

    teste refatorando 1 test

--- Calculadora.js
+++ Calculadora.js
@@ -1,5 +1,5 @@
 export function calculadora(emp){
-    if(emp.role === "DESENVOLVEDOR"){
+    if(emp.role == "DESENVOLVEDOR"){
         if(emp.salary > 3000 ){
             return emp.salary * 0.80
         }
