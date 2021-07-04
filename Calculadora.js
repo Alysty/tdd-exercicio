ddaf6969838a15264c14b9693078225559f18483
Alysty <pppjw@protonmail.ch>
2021-07-04 18:32:27 -0300

    teste refatorando 5 6 and 7 tests

--- Calculadora.js
+++ Calculadora.js
@@ -2,18 +2,22 @@ export function calculadora(emp){
     if(typeof (emp.salary)!="number"){
         throw "Salario deve ser um numero"
     }
-    if(emp.role == "DESENVOLVEDOR"){
-        if(emp.salary >= 3000 ){
-            return emp.salary * 0.80
-        }else{
-            return emp.salary * 0.90
-        }
+    switch(emp.role){
+        case "DESENVOLVEDOR":
+            if(emp.salary >= 3000 ){
+                return emp.salary * 0.80
+            }else{
+                return emp.salary * 0.90
+            }
+            break;
+        case "DBA":
+            if(emp.salary >= 2000 ){
+                return emp.salary * 0.75
+            }else{
+                return emp.salary * 0.85
+            }
+            break;
     }
-    if(emp.role == "DBA"){
-        if(emp.salary >= 2000 ){
-            return emp.salary * 0.75
-        }else{
-            return emp.salary * 0.85
-        }
-    }
+
+
 }
