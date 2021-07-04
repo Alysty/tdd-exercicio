cf010bc4dfd2f483e0835a2022869c3598347c62
Alysty <pppjw@protonmail.ch>
2021-07-04 18:18:40 -0300

    teste passando 2 test

--- __tests__/Tests.spec.js
+++ __tests__/Tests.spec.js
@@ -3,7 +3,8 @@ import {calculadora} from "../Calculadora"
 
 test('Deve retornar um erro informado que o salario deve ser um numero ',()=>{
     const emp = new Funcionarios("name", "email", "5000", "DESENVOLVEDOR")
-    expect(calculadora(emp)).toThrow("Salario deve ser um numero")
+    expect(()=>{calculadora(emp)}).toThrow("Salario deve ser um numero")
+
 })
 
 test('Deve retornar o salario liquido de um DESENVOLVERDOR com um é salario maior que 3000',()=>{
