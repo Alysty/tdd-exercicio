651970ad5ce654c0f9132fa3aaef480729b80db6
Alysty <pppjw@protonmail.ch>
2021-07-04 17:59:43 -0300

    teste falhando 2 test

--- __tests__/Tests.spec.js
+++ __tests__/Tests.spec.js
@@ -1,6 +1,11 @@
 import Funcionarios from "../Funcionarios"
 import {calculadora} from "../Calculadora"
 
+test('Deve retornar um erro informado que o salario deve ser um numero ',()=>{
+    const emp = new Funcionarios("name", "email", "5000", "DESENVOLVEDOR")
+    expect(calculadora(emp)).toThrow("Salario deve ser um numero")
+})
+
 test('Deve retornar o salario liquido de um DESENVOLVERDOR com um é salario maior que 3000',()=>{
     const emp = new Funcionarios("name", "email", 5000, "DESENVOLVEDOR")
     expect(calculadora(emp)).toBe(4000)
